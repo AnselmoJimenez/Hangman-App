504260baae7efe515d911aadbd2fa1964ec6b31d
AnselmoJimenez <anselmojimenez0@gmail.com>
2023-06-17 15:12:19 -0400

    Add project files.

---
+++ Word.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_App
+{
+    internal class Word
+    {
+        private string word { get; }
+        private char[] letters;
+        public Word() 
+        {
+            this.word = string.Empty;
+            this.letters = new char[this.word.Length];
+        }
+    }
+}
